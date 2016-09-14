380994c12881b8ef0032341b05bb2056489d7005
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-09-13 17:45:13 -0700

    implemented autocomplete functionality

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
+import React from 'react';
+import {Component} from 'react';
 import AutoComplete from 'material-ui/AutoComplete';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import JSONP from 'jsonp';
 
 
-const googleAutoSuggestURL =  `
-  //suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=`;
+const googleAutoSuggestURL = `//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=`;
 
 
 class App extends React.Component{
@@ -31,14 +31,15 @@ class App extends React.Component{
         });
         }
 
-    performSearch(){
+    performSearch() {
         var
             self = this,
             url  = googleAutoSuggestURL + this.state.inputValue;
 
-        if(this.state.inputValue === '') {
+
+        if(this.state.inputValue !== '') {
             JSONP(url, function(error, data) {
-                var searchResults, retrievedSearchItems;
+                var searchResults, retrievedSearchTerms;
 
                 if(error) return error;
 
